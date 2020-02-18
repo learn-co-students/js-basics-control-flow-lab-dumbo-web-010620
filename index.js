3ebdabb7effd7bf3811663a846bc158fdf7d8d10
wenglin1 <wenglin@buffalo.edu>
2020-02-18 11:54:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let xx
+  if (feet < 400) {
+    xx = 'This one is on me!'
+  }
+  else if (feet > 2000 && feet < 2500) { 
+    xx = 'I will gladly take your thirty bucks.'
+  }
+  else { 
+    xx = 'No can do.'
+  }
+  return xx
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let xx
+  if (city === 'NYC') {
+    xx = 'Ok, sounds good.'
+  }
+  else {
+    xx = 'No go.'
+  }
+  return xx
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
